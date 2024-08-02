0756703a0ec42c7668f5398df84d0ba044fa40e2
VIA2NEY <kouameangeV11@gmail.com>
2024-08-02 19:26:18 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:day_15_shopping_app_ui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage()
+  )
+);
